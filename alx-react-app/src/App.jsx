93a97e9a3bf73ed7c8a4c0215ff73e25454981ff
Ster <acetynne@gmail.com>
2025-07-13 14:12:57 +0300

    react

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import WelcomeMessage from './WelcomeMessage.jsx';
+import Header from './Header.jsx';
+import MainContent from './MainContent.jsx';
+import Footer from './Footer.jsx';
+
+function App() {
+  return(
+    <>
+    <WelcomeMessage/>
+    <Header/>
+    <MainContent/>
+    </>
+
+  );
+
+}
+
+export default App;
