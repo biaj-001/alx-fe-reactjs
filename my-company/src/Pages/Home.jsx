596141f8bea7b898a6bff591c5d4a784684af6fa
biaj-001 <acetynne@gmail.com>
2025-07-20 22:11:37 +0300

    Update Home.jsx

--- my-company/src/Pages/Home.jsx
+++ my-company/src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 function Home() {
      return (
-       <div style={{ padding: '20px' }}>
-         <h1>Welcome to Our Company</h1>
-         <p>We are dedicated to delivering excellence in all our services.</p>
+       <div style={{ padding: '20px',backgroundColor:'#f0f0f0' }}>
+         <h1 style={{color:'blue'}}>Welcome to Our Company</h1>
+         <p style={{fontFamily:'Arial'}}>We are dedicated to delivering excellence in all our services.</p>
        </div>
      );
    }
