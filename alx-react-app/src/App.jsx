6cad0851ab2f9aab874f04ed15610e313c773f81
biaj-001 <acetynne@gmail.com>
2025-07-20 23:01:06 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-import WelcomeMessage from './components/WelcomeMessage.jsx';
+// import WelcomeMessage from './components/WelcomeMessage.jsx';
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
-import UserProfile from './components/UserProfile.jsx';
+// import UserProfile from './components/UserProfile.jsx';
 
 
 function App() {
   return(
    <>
-    <WelcomeMessage/>
+{/*     <WelcomeMessage/> */}
     <Header/>
     <MainContent/>
     <Footer/>
-    <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
+{/*     <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/> */}
    </>
 
   );
