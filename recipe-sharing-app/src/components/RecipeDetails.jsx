7224f13bae5d14649e836e4cda7587a71b9117af
biaj-001 <acetynne@gmail.com>
2025-07-26 23:43:39 +0300

    Update RecipeDetails.jsx

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,23 +1,20 @@
 import { useParams } from 'react-router-dom';
-import { useRecipeStore } from '../store/recipeStore';
+import { useRecipeStore } from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
-import FavoriteToggleButton from './FavoriteToggleButton';
 
 const RecipeDetails = () => {
   const { id } = useParams();
-  const recipeId = parseInt(id);
   const recipe = useRecipeStore((state) =>
-    state.recipes.find((r) => r.id === recipeId)
+    state.recipes.find((r) => r.id === id)
   );
 
-  if (!recipe) return <p>Recipe not found.</p>;
+  if (!recipe) return <p>Recipe not found</p>;
 
   return (
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
-      <FavoriteToggleButton recipeId={recipe.id} />
       <EditRecipeForm recipe={recipe} />
       <DeleteRecipeButton id={recipe.id} />
     </div>
@@ -25,3 +22,4 @@ const RecipeDetails = () => {
 };
 
 export default RecipeDetails;
+
