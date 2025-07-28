d33d964a264014b528b213dddcd4e9bf51b92d08
Ster <acetynne@gmail.com>
2025-07-28 19:41:12 +0300

    Modify

--- my-company/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ textAlign: 'center', marginTop: '30px', padding: '10px', backgroundColor: '#f0f0f0' }}>
-      <p>&copy; {new Date().getFullYear()} My Company. All rights reserved.</p>
-    </footer>
-  );
-}
-export default Footer;
