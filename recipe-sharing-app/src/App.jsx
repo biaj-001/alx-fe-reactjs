6f71b95c54892fafdb22f3ac9529d87caf941c6d
biaj-001 <acetynne@gmail.com>
2025-07-28 14:11:47 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,15 @@
-import AddRecipeForm from './component/AddRecipeForm.jsx';
-import RecipeList from './component/RecipeList.jsx';
-import FavoritesList from './component/FavoritesList.jsx';
-import RecommendationsList from './component/RecommendationsList.jsx';
+import AddRecipeForm from './component/AddRecipeForm';
+import RecipeList from './component/RecipeList';
 
-const App = () => {
+function App() {
   return (
-    <div className="max-w-4xl mx-auto p-4 space-y-6">
-      <h1 className="text-3xl font-bold"> Recipe Sharing App</h1>
+    <div className="max-w-xl mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
-      <FavoritesList />
-      <RecommendationsList />
     </div>
   );
-};
+}
 
 export default App;
+
