e7d45fdadeb84541c980b38e35c39544d390b88a
biaj-001 <acetynne@gmail.com>
2025-08-04 15:20:09 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -5,7 +5,6 @@ export const fetchUserData = async (username) => {
     const response = await axios.get(https://api.github.com/users/${username});
     return response.data;
   } catch (error) {
-    // This custom error message is key for the 'API request handling' test
     throw new Error("Looks like we can't find the user.");
   }
 };
